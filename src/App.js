83db808cec8cd404d5a71ec9adc20fe83c950690
Mindy D Tran <mindytran98@gmail.com>
2024-01-30 15:50:09 -0500

    passing movie prop to movie display

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Form from "./components/Form";
 import MovieDisplay from "./components/MovieDisplay";
 
 function App() {
-  const [movies, setMovies] = useState(null);
+  const [movie, setMovie] = useState(null);
 
   const apiKey = "c5a9bddf";
   const url = `http://www.omdbapi.com/?apikey=${apiKey}`;
@@ -16,13 +16,13 @@ function App() {
     const data = await res.json();
     console.log(data);
     // setting Movie state to recieved data
-    setMovies(data);
+    setMovie(data);
   };
 
   return (
     <div className="App">
       <Form moviesearch={getMovie} />
-      <MovieDisplay />
+      <MovieDisplay movie={movie}/>
     </div>
   );
 }
