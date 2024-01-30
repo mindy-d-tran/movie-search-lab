e0e6a8ec45573c4e76b5edef7ffaea85bd712248
Mindy D Tran <mindytran98@gmail.com>
2024-01-30 15:54:55 -0500

    add functions to deal with if movie is empty or not

--- src/components/MovieDisplay.js
+++ src/components/MovieDisplay.js
@@ -1,4 +1,23 @@
-export default function MovieDisplay(props) {
-  // The component must return some JSX
-  return <h1>The MovieDisplay Component</h1>;
+export default function MovieDisplay({ movie }) {
+  //function to return loaded JSX
+  const loaded = () => {
+    return (
+      <>
+        <h1>{movie.Title}</h1>
+        <h2>{movie.Genre}</h2>
+        <img src={movie.Poster} alt={movie.Title} />
+        <h2>{movie.Year}</h2>
+      </>
+    );
+  };
+  //function to return loading JSX
+  const loading = () => {
+    return (
+      <>
+        <h1>No movie to display</h1>
+      </>
+    );
+  };
+
+  return movie ? loaded() : loading();
 }
